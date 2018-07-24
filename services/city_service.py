57616676abf31df5e9c7c77ef9732e871cb50661
GuillViegas <guivfaria@gmail.com>
2018-07-24 18:15:54 -0300

    Classe gerência transações envolvendo tabela cidades

---
+++ services/city_service.py
@@ -0,0 +1,15 @@
+from dbconnection import DBconnection
+
+class CityService:
+	_db = None
+
+	def __init__(self):
+		self._db = DBconnection()
+
+	def getCityByName(self, name):
+		sql = "SELECT * FROM cidades c WHERE c.nome = '" + name + "'"
+		return self._db.squery(sql)
+
+	def postCity(self, city, state):
+		sql = "INSERT INTO cidades (nome, estado_id) VALUES  ('" + city + "', "+str(state)+") RETURNING *"
+		return self._db.pquery(sql)
