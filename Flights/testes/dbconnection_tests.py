fd412e45de7452024e1ce3c4392993cec996963c
GuilViegas <guivfaria@gmail.com>
2018-07-30 00:21:16 -0300

    Remoção de arquivos versão antiga do servidor

--- Flights/testes/dbconnection_tests.py
+++
@@ -1,8 +0,0 @@
-import unittest
-from Flights.service.dbconnection import DBconnection
-
-class DBConnectionTests(unittest.TestCase):
-	def squery_test(self):
-
-if __name__ == '__main__':
-    unittest.main()
